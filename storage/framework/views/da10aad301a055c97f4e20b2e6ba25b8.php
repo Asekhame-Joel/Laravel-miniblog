ed1c1b58235c267e7c67376ff42560a4b5130886
asekhame <asekhamejoel@gmail.com>
2024-05-03 20:47:32 +0100

    finished project

--- storage/framework/views/da10aad301a055c97f4e20b2e6ba25b8.php
+++
@@ -1,5 +0,0 @@
-<div class="bg-gray-100 border border-gray-200 p-6 rounded-xl">
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /home/linuxbrew/.linuxbrew/etc/php/laravelblog/resources/views/components/panel.blade.php ENDPATH**/ ?>
