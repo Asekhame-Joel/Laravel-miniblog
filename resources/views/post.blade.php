520a89cb79c1d573ea3cb28693dd76644bff2115
asekhame <asekhamejoel@gmail.com>
2024-05-06 11:30:54 +0100

    Updated routes and created a post folder to get posts

---
+++ resources/views/post.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+
+<body>
+    <article>
+    <?php echo  $post ?>
+</article>
+<a href="/">Go Back</a>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,78 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function($slug){
+//the slug here represents the value of the post from the URL
+$path = __DIR__ . "/../resources/posts/{$slug}.html";
+if(! file_exists($path)){
+    abort(404);
+}
+
+$post = cache()->remember("posts.{$slug}", 5, function () use ($path){
+    var_dump('file_get_contents');
+    return  file_get_contents($path);
+});
+
+return view('post', [
+    //here the post which represents the post.blade file.. 
+    //is posted and renders a view from the $post which 
+    //has a slug of whaterver is rendered in the URL when it is requested
+    'post' => $post
+]);
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Route::get('posts/{post}', function ($slug){
+//     //the slug represents the value from the {post}
+
+//     $path = __DIR__ . "/../resources/posts/{$slug}.html";
+//     if(! file_exists($path)){
+//         abort(404);
+//     }
+//     $post = file_get_contents($path);
+//     return view('post', [
+//         //the post here represent the blade file post.blade
+//         'post' => $post
+//     ]);
+// });
+
+
+
